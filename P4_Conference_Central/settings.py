c2c74bd5eec6adab53df3bca2fabebbce381d351
Itsuo Okamoto <itsuo.okamoto@recto.com>
2015-12-11 21:13:59 -0800

    P4 Conference Center check in

---
+++ P4_Conference_Central/settings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = '246167447429-mpl2hspm0i3lsu4hkgp564tq1ebi7p5s.apps.googleusercontent.com'
+ANDROID_CLIENT_ID = 'replace with Android client ID'
+IOS_CLIENT_ID = 'replace with iOS client ID'
+ANDROID_AUDIENCE = WEB_CLIENT_ID
